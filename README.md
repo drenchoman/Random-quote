fbe6823de52e6424285cf47674117be7e014f12f
Oscar Harron <oscarharron@hotmail.com>
2022-08-08 17:49:34 +1200

    add random-quote generator

---
+++ README.md
@@ -0,0 +1,8 @@
+# Random Quote Generator
+
+
+## To Use
+NPM i - to install dependencies
+node index.js to generate a quote
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const all = require('everyday-fun')
+
+let quote = all.getRandomQuote()
+
+console.log(quote.quote)
+console.log(`~~${quote.author}`)
